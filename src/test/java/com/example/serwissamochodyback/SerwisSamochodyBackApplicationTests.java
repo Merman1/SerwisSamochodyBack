cbac77613ee7537b5bc5f9e81a4a3b06bb05df33
Merman1 <jarek003514@outlook.com>
2023-11-08 11:22:26 +0100

    Starting a project

---
+++ src/test/java/com/example/serwissamochodyback/SerwisSamochodyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serwissamochodyback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerwisSamochodyBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
